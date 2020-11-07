fee8f09ddacaea80909b04c456237f4b87dc0c6d
luozihao <3136511148@qq.com>
2020-11-07 15:31:45 +0800

    idea提交

---
+++ cloud-client/src/test/java/com/cloud/client/CloudClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cloud.client;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cloud-provider/src/test/java/com/cloud/provider/CloudProviderApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cloud.provider;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
